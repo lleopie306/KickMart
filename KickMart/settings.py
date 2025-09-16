a46d06986ca6adf95c5aae63b0c801785453fe4f
lleopie306 <desver4@gmail.com>
2025-09-16 23:00:55 +0700

    tugas 3 belum ada readme.md

--- KickMart/settings.py
+++ KickMart/settings.py
@@ -32,7 +32,9 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "deslee-jever-kickmart.pbp.cs.ui.ac.id"]
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://deslee-jever-kickmart.pbp.cs.ui.ac.id"
+]
 
 # Application definition
 
@@ -61,7 +63,7 @@ ROOT_URLCONF = 'KickMart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
